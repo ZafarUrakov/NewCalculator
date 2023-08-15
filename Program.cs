f852b74b73dfbb3d8cdea8c42296a64fc26803d8
ZafarUrakov <goliburakov535@gmail.com>
2023-08-14 23:45:12 -0700

    Refactor OPP

--- Program.cs
+++ Program.cs
@@ -2,27 +2,47 @@
 
 namespace Calculator
 {
-    class Program
+    class Program // Exposer - tahtim kiluvchi qavat
     {
          static void Main()
-        {
-            int choiceOfOptions = 0;
+         { string backToMenu;
             do
             {
-                var calculator = new CalculatR();
-                var multiplicator = new MultiplicatR();
-                MessageR.ShowMenu();
-                choiceOfOptions = MessageR.MessageErrors();
+                ValueManipulatR.ShowMenu();
+                int choiceOfOptions = ReportR.ConvertInputOption();
                 if (choiceOfOptions == 1)
-                    calculator.ShowCalculator();
+                {
+                    decimal firstValue = ValueManipulatR.GetUserValueByMassage("Welcome to Calculator" +
+                                                                               "\n\tPRC - percentage counter" +
+                                                                               "\nFirst number: ");
+                    string actionValue = ValueManipulatR.GetInputByMessageToAction("\nOperation[+ | - | * | / | % | prc]: ");
+                    if (actionValue == "prc")
+                    {
+                        decimal secondValue = ValueManipulatR.GetUserValueByMassage("\nPercent: ");
+                        var calculate = new AdvancedCalculatR(firstValue, actionValue, secondValue);
+                        calculate.ShowCalculator();
+                    }
+                    else if (actionValue == "+" || actionValue == "-" || actionValue == "*" || actionValue == "/" || actionValue == "%")
+                    {
+                        decimal secondValue = ValueManipulatR.GetUserValueByMassage("\nSecond number: ");
+                        var calculator = new CalculatR(firstValue, actionValue, secondValue);
+                    }
+                    else
+                        Console.WriteLine("There is no such choice.Try again.");
+
+                }
                 else if (choiceOfOptions == 2)
-                    multiplicator.ShowMultiplicationTable();
+                {
+                    var multiplicator = new MultiplicatR();
+                }
                 else if (choiceOfOptions == 3)
                 {
-                    MessageR.Exit();
+                    ReportR.Exit();
                     break;
                 }
-            } while (choiceOfOptions);
+                backToMenu = ValueManipulatR.BackToMenu();
+            } while (backToMenu == "yes");
+            Console.WriteLine("GoodBye!");
         }
     }
 }
