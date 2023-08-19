afbc8cf6a339409c7b79715b31e9683a2a4154a8
ZafarUrakov <goliburakov535@gmail.com>
2023-08-18 22:41:58 -0700

    Business Logic Refactored

---
+++ Classes/BusinessLogic/MultiplicatR.cs
@@ -0,0 +1,26 @@
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Calculator.Classes;
+
+public class MultiplicatR// business logic
+{
+    IReportingContract reporter = new ReportR();
+    private decimal number;
+    private decimal loopVariable;
+
+    public MultiplicatR()
+    {
+        number = ValueManipulatR.GetUserValueByMassage("Multiplication table\nNumber to calculate: ");
+        MultiplyWork();
+    }
+
+    private void MultiplyWork()
+    { reporter.ResultProgress();
+        for (int minorNumber = 1; minorNumber <= 9; minorNumber++)
+        {
+            loopVariable = number;
+            loopVariable *= minorNumber;
+            Console.WriteLine($"{number} * {minorNumber} = {loopVariable}");
+        }
+    }
+}
